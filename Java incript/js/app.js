81f45caef6920f305daeb4a558dfdd3cc6312fd1
Your Name <you@example.com>
2022-07-28 14:55:17 -0300

    proyecto final

--- Java incript/js/app.js
+++ Java incript/js/app.js
@@ -189,3 +189,16 @@ const btnsuma = e => {
         SelecionarCarrito()
     }
 }
+let url = 'https://jsonplaceholder.typicode.com/users/';
+        fetch(url)
+            .then( response => response.json() )
+            .then( data => mostrarData(data) )
+            .catch( error => console.log(error) )
+
+        const mostrarData = (data) => {
+            console.log(data)
+            }
+
+
+
+
